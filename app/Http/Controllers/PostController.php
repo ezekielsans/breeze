25f7cb54a1825bbc19a61b1a2952f8d613193fa7
ezekielsans <ezekielsantos.dev@gmail.com>
2024-07-29 14:09:15 +0800

    added PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PostController extends Controller
+{
+    /**
+     * Display
+     * @return view
+     * */
+
+    public function create()
+    {
+
+//return
+
+    }
+}
